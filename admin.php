27e6d619e7651b069d6e2d203b08737aaa6e12f9
jerem512 <jeremymarchal90@gmail.com>
2019-01-07 12:00:30 +0100

    commit 2

---
+++ admin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Connexion</title>
+</head>
+<body>
+	<h1>Veuillez vous connecter en tant qu'admin :</h1>
+	<form action="co.php">
+		<label for="login">Nom de compte :</label>
+		<input type="text" placeholder="Nom de compte" id="login" name="login">
+		<label for="pass">Mot de passe :</label>
+		<input type="password" placeholder="Mot de passe" id="pass" name="pass">
+		<button>Se connecter</button>
+	</form>
+</body>
+</html>
